d5d638c9278fea977b961a264882ddc0ca23496d
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-12-17 10:39:52 +0200

    Change static and installed apps

--- real_estate_broker/settings.py
+++ real_estate_broker/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_app'
+    'real_estate_broker.main_app'
 ]
 
 MIDDLEWARE = [
@@ -122,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
