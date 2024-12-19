61fb1cfbf14b53b7c671b81c20f69cb27499ea66
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-12-19 16:25:15 +0200

    Update main app

--- real_estate_broker/main_app/apps.py
+++ real_estate_broker/main_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main_app'
+    name = 'real_estate_broker.main_app'
--- real_estate_broker/main_app/views.py
+++ real_estate_broker/main_app/views.py
@@ -1,33 +1,8 @@
 from django.shortcuts import render
-from .models import PersonalInfo, AdvertisementInfo, Video, Comment
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-def home(request):
-    personal_info = PersonalInfo.objects.first()
-    title = personal_info.title
-    description = personal_info.description
-    picture_path = personal_info.picture
-
-    adv_info = AdvertisementInfo.objects.first()
-    adv_title = adv_info.title
-    adv_description = adv_info.description
-    gif_path = adv_info.video
-
-    video = Video.objects.first()
-    open_video = video.main_video
-
-    comments = Comment.objects.all()
-
-    context = {
-        'title': title,
-        'description': [text for text in description.split('\n')],
-        'picture_path': picture_path,
-        'adv_title': adv_title,
-        'adv_description': [text for text in adv_description.split('\n')],
-        'gif_path': gif_path,
-        'open_video': open_video,
-        'comments': comments
-    }
-    return render(request, 'home.html', context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
