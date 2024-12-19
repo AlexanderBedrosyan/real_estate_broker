dec14f9f40b1b58e82c256e566c9caaed4db93f7
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-12-19 20:19:07 +0200

    Update models, views, migration and home page

--- real_estate_broker/main_app/models.py
+++ real_estate_broker/main_app/models.py
@@ -2,3 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
+class Comments(models.Model):
+    first_name = models.CharField(
+        max_length=100
+    )
+    last_name = models.CharField(
+        max_length=100
+    )
+    profession = models.CharField(
+        max_length=100
+    )
+    picture_url = models.URLField()
+    short_message = models.CharField(
+        max_length=1000
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- real_estate_broker/main_app/views.py
+++ real_estate_broker/main_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Comments
 
 # Create your views here.
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        comments = list(Comments.objects.all())
+        first_comment = None
+        if comments:
+            first_comment = comments.pop()
+        context['comments'] = comments
+        context['first_comment'] = first_comment
+
+        return context
