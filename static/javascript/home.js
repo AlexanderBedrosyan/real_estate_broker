05076e317193e85aa0bb47332e6823572921f401
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-12-20 21:34:53 +0200

    Update home page to use correctly the video, add contacts html and css

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -19,3 +19,28 @@ modalElement.addEventListener('hidden.bs.modal', () => {
     const miniTvIframe = document.getElementById('mini-tv-video');
     miniTvIframe.src = `https://www.youtube.com/embed/${miniTvVideoId}?autoplay=1&mute=1&loop=1&playlist=${miniTvVideoId}`;
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const mediaElements = document.querySelectorAll("audio, video");
+    mediaElements.forEach(element => {
+        element.muted = true;
+    });
+
+    const iframeElements = document.querySelectorAll("iframe");
+    iframeElements.forEach(iframe => {
+        const src = iframe.src;
+        if (src.includes("youtube.com/embed")) {
+            const mutedSrc = appendMuteToYouTubeURL(src);
+            iframe.src = mutedSrc;
+        }
+    });
+});
+
+
+function appendMuteToYouTubeURL(url) {
+    const urlObj = new URL(url);
+    urlObj.searchParams.set("mute", "1"); 
+    return urlObj.toString();
+}
