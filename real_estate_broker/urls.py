64c73a8a23772a30fe278b040bfc922793d3af5b
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-12-24 17:46:31 +0200

    Add new path in main urls and update home page

--- real_estate_broker/urls.py
+++ real_estate_broker/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('real_estate_broker.main_app.urls')),
+    path('common/', include('real_estate_broker.common.urls')),
 ]
