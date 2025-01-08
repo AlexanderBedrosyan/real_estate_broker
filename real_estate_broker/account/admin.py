5fdf1b8c57454e38628a844eed8f5562fcfa4fb7
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-01-08 11:52:21 +0200

    Create Customer admin panel

--- real_estate_broker/account/admin.py
+++ real_estate_broker/account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomerModel
 
 # Register your models here.
+
+
+@admin.register(CustomerModel)
+class CustomerModelAdmin(admin.ModelAdmin):
+    pass
