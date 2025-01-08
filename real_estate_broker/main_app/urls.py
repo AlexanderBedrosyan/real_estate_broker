10f49f3d5038aff0bd6c502fa63433942efda24c
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-01-08 11:51:46 +0200

    Fix details event url

--- real_estate_broker/main_app/urls.py
+++ real_estate_broker/main_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('contacts', views.ContactsView.as_view(), name='contacts'),
     path('consultation', views.ConsultationView.as_view(), name='consultation'),
     path('events', views.EventsView.as_view(), name='events'),
-    path('event-details//<int:pk>', views.EventsDetailsView.as_view(), name='event-details'),
+    path('event-details/<int:pk>', views.EventsDetailsView.as_view(), name='event-details'),
     path('invest-with-me', views.InvestWithMeView.as_view(), name='invest-with-me'),
 ]
