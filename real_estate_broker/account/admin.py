f4175c6c7db1c7fa2dcba80d7ec3cdb89d0ce9b0
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-01-08 12:16:31 +0200

    Add contact in admin panel

--- real_estate_broker/account/admin.py
+++ real_estate_broker/account/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import CustomerModel
+from .models import CustomerModel, Contact
 
 # Register your models here.
 
 
 @admin.register(CustomerModel)
 class CustomerModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
     pass
