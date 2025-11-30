c2a7557c93d57c3f5a29cfe818769d8398e77240
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-11-30 17:47:17 +0200

    Update

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Копирай кода и entrypoint
+# Копирай останалия код и entrypoint
 COPY . .
 
 # Направи entrypoint изпълним
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 # Скрипт за изчакване на базата преди стартиране на Django
 
+echo "DB_HOST=$DB_HOST"
+echo "DB_PORT=$DB_PORT"
+echo "DB_NAME=$DB_NAME"
+echo "DB_USER=$DB_USER"
+echo "DB_PASSWORD=$DB_PASSWORD"
+
 echo "Waiting for database to be ready..."
 
 while ! python - <<END
 import psycopg2, os
 try:
     conn = psycopg2.connect(
-        host=os.environ.get("DB_HOST", "db"),
-        port=int(os.environ.get("DB_PORT", 5432)),
-        database=os.environ.get("DB_NAME", "postgres"),
-        user=os.environ.get("DB_USER", "postgres"),
-        password=os.environ.get("DB_PASSWORD", "postgres")
+        host=os.environ["DB_HOST"],
+        port=int(os.environ["DB_PORT"]),
+        database=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"]
     )
     conn.close()
     exit(0)
