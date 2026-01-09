a1d27cb794c53b5422c912d75abed57d4828e7ba
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2026-01-09 10:29:16 +0200

    Update settings

--- real_estate_broker/settings.py
+++ real_estate_broker/settings.py
@@ -146,6 +146,10 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Media files (uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
