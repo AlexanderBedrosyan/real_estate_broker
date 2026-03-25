646c4defdd6e865fd40a0e9e0e99ceeba99ae733
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2026-03-25 10:24:33 +0200

    Serve media files in production via Django serve view

--- real_estate_broker/urls.py
+++ real_estate_broker/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.sitemaps.views import sitemap
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from real_estate_broker.main_app.sitemaps import StaticViewSitemap, EventSitemap, ProjectSitemap
 
 sitemaps = {
@@ -21,4 +22,6 @@ urlpatterns = [
 
 handler404 = "real_estate_broker.main_app.views.error_404_view"
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
